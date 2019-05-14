53196a950143b92fcb1a6b164e1ec66f3e9c5024
joaovitorblabres <joaovitorlabres@gmail.com>
2019-05-14 14:46:03 -0300

    Creio que esteja pronto

--- main.cpp
+++ main.cpp
@@ -1,17 +1,7 @@
-/**
- * A simple OpenGL program that draws a colorful cube
- * that rotates as you move the arrow keys.
- *
- * Author: Mihalis Tsoukalos
- * Date: Wednesday 04 June 2014
- */
-
 #include <GL/glut.h>
 #include <GL/gl.h>
 #define WIDTH 800
 #define HEIGHT 600
-
-// The coordinates for the vertices of the cube
 
 static void drawFace(GLfloat v[4][3], GLfloat RGB[3]){
 		glColor3f(RGB[0], RGB[1], RGB[2]);
@@ -57,10 +47,10 @@ public:
 	}
 	void update(){
 		glPushMatrix();
-			glRotatef(xRo += this->xRotate, 1.0f, 0.0f, 0.0f);
-			glRotatef(yRo += this->yRotate, 0.0f, 1.0f, 0.0f);
 			glRotatef(rRe += this->revolution, 0.0f, 1.0f, 0.0f);
 			glTranslatef(0, 0, this->zTrans);
+			glRotatef(xRo += this->xRotate, 1.0f, 0.0f, 0.0f);
+			glRotatef(yRo += this->yRotate, 0.0f, 1.0f, 0.0f);
 			for(int i = 0; i < 6; i++){
 				drawFace(this->vertexes[i], this->RGB[i]);
 			}
@@ -68,52 +58,33 @@ public:
 	}
 };
 
-cube center = cube(1.f, 1.f, 1.f, 0, 3, 0, 0);
+cube sol = cube(1.5f, 1.5f, 1.5f, 0, 3, 0, 0);
+cube terra = cube(1.f, 1.f, 1.f, 1, 0, -1, 15);
 
 void display(){
-	// Set Background Color
 	glClearColor(0.4f, 0.4f, 0.4f, 1.0f);
-	// Clear screen
+
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	center.update();
-	// Reset transformations
+
 	glLoadIdentity();
+
+	sol.update();
+	terra.update();
 
 	glFlush();
 	glutSwapBuffers();
 }
 
-void keyboard(int key, int x, int y){
-/*	if (key == GLUT_KEY_RIGHT) {
-		rY += 5;
-	} else if (key == GLUT_KEY_LEFT) {
-		rY -= 5;
-	} else if (key == GLUT_KEY_DOWN) {
-		rX -= 5;
-	} else if (key == GLUT_KEY_UP) {
-		rX += 5;
-	}*/
-
-	// Request display update
-	glutPostRedisplay();
-}
-
-
-
 int main(int argc, char **argv){
-	// Initialize GLUT and process user parameters
 	glutInit(&argc, argv);
 
-	// Request double buffered true color window with Z-buffer
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 
 	glutInitWindowSize(WIDTH, HEIGHT);
 	glutInitWindowPosition(100, 100);
 
-	// Create window
 	glutCreateWindow("OpenGL Cube");
 
-	// Enable Z-buffer depth test
 	glEnable(GL_DEPTH_TEST);
 
 	glViewport(0, 0, (GLsizei)WIDTH, (GLsizei)HEIGHT);
@@ -122,15 +93,12 @@ int main(int argc, char **argv){
 
 	glLoadIdentity();
 	glFrustum(-1.0, 1.0, -1.0, 1.0, 1.5, 100.0);
-	glTranslatef(0, -5.0, -30.0f); // "ALTERA" VISÃO DA "CAMERA"
+	glTranslatef(0, -8.0, -30.0f); // "ALTERA" VISÃO DA "CAMERA"
 
 	glMatrixMode(GL_MODELVIEW);
-	// Callback functions
 	glutDisplayFunc(display);
-	glutSpecialFunc(keyboard);
 	glutIdleFunc(display);
 
-	// Pass control to GLUT for events
 	glutMainLoop();
 
 	return 0;
